563ad0d565386291f8febd6cdd442eebd02d0df0
landsolutionin <sonu26bolg@gmail.com>
2026-07-10 00:10:26 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,4 @@
 const firebaseConfig = {
-    // আপনার ফায়ারবেস কনসোল থেকে পাওয়া আইডিগুলো এখানে বসান
-const firebaseConfig = {
     apiKey: "AIzaSyABwusy3oZXqh3531oJlQorBsUMWxQF08I",
     authDomain: "live-result-b9155.firebaseapp.com",
     databaseURL: "https://live-result-b9155-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -10,6 +8,5 @@ const firebaseConfig = {
     appId: "1:495121483481:web:8e8bf65c71ea3d31ec60c8",
     measurementId: "G-DFDW40QF87"
 };
-
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
